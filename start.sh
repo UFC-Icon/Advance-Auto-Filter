7b4693704f3c7ba60fe694e47bb23d9e17f4dc6e
UFC-Icon <139276613+UFC-Icon@users.noreply.github.com>
2025-05-19 12:00:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone MukeshDeety/Advance-Auto-Filter /Advance-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
-fi
-cd /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO/Advance-Auto-Filter
+fifi
+cdcd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
